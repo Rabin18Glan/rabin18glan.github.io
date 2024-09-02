e76cda8729f637bf674ec366522bc83d3a752c3e
Rabin Glan <robinjsl321@gmail.com>
2024-09-02 19:30:24 +0545

    some changes for metadata

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -1,8 +1,17 @@
 import { HeroContainer, ProfileImage, ProfileInfo } from "../features/hero";
-
+import Helmet from 'react-helmet';
 function Hero() {
   return (
     <HeroContainer>
+          <Helmet>
+        <title>Rabin Glan | Crafting Digital Experiences</title>
+        <meta name="description" content="Elevating web development with a blend of creativity and technical expertise. Based in Hetauda, I specialize in building dynamic and engaging digital experiences." />
+        <meta property="og:title" content="Rabin Glan | Crafting Digital Experiences" />
+        <meta property="og:description" content="Turning ideas into impactful web experiences. Fullstack Developer with a passion for innovation and quality." />
+        <meta property="og:image" content="/profile.jpg" />
+        <meta property="og:url" content="https://rabin18glan.github.io/" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Helmet>
       <ProfileImage />
       <ProfileInfo />
     </HeroContainer>
