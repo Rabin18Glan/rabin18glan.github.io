cfe24cbf74a4329b86c6e95d7d9ffec5ec34bde5
Rabin Glan <robinjsl321@gmail.com>
2024-09-04 07:31:08 +0545

    new project added

--- src/features/projects/data/projectListData.ts
+++ src/features/projects/data/projectListData.ts
@@ -20,14 +20,14 @@ export const ProjectListData:ProjectProps[]=[{
     projectType:'Personal',
     name:"Chat app",
     type:'Chat',
-    technologiesUsed:['react','laravel','mysql'],
+    technologiesUsed:['react','node','mongodb','express','tailwind'],
     images:['/chat.png'],
     description:"This is a simple chat app build using React, Node/Express and MongoDB. This project is for learning React and Socket.io",
     taskCompleted:['frontend development with React','backend with Node/Express','client side and server side validations','Sockect.io connection between client and server'],
     urlBtnLinks:[
         {
             name:"View Code",
-            link:''
+            link:'https://github.com/Rabin18Glan/MERN-simple-chat-app'
         },
         {
             name:"More Info",
@@ -42,7 +42,7 @@ export const ProjectListData:ProjectProps[]=[{
     projectType:'Personal',
     name:"Maruni Suppliers",
     type:'Ecommerce Platform',
-    technologiesUsed:['react','laravel','mysql'],
+    technologiesUsed:['react','laravel','mysql','tailwind','php','javascript'],
     images:['/react-laravel.png'],
     description:`This is the ecommerce website `,
     taskCompleted:['frontend development with react','backend with laravel'],
@@ -50,11 +50,32 @@ export const ProjectListData:ProjectProps[]=[{
     [
         {
             name:'View Code',
+            link:'https://github.com/Rabin18Glan/react-laravel-ecom-project'
+        },
+        {
+            name:"More Info",
             link:''
+        }
+    ]
+  
+  },{
+    id:3,
+    projectType:'',
+    name:"Laliguras Bee Farm",
+    type:'Ecommerce Platform',
+    technologiesUsed:['next','react','tailwind'],
+    images:['/laliguras.png'],
+    description:`This is the ecommerce website for buying Honey `,
+    taskCompleted:['frontend development with next','backend with next'],
+    urlBtnLinks:
+    [
+        {
+            name:'View Code',
+            link:'https://github.com/Rabin18Glan/laliguras'
         },
         {
             name:"Visit Site",
-            link:''
+            link:'https://rabin18glan.github.io/laliguras/'
         }
     ]
   
