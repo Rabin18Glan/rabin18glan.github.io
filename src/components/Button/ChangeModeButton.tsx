6045eb469196eae650a97a8210c0a3306d593d5e
Rabin Glan <robinjsl321@gmail.com>
2025-04-05 11:19:40 +0545

    porfile images changed and color context configured

--- src/components/Button/ChangeModeButton.tsx
+++ src/components/Button/ChangeModeButton.tsx
@@ -1,17 +1,16 @@
 import { Brightness2Outlined, Brightness7Rounded } from '@mui/icons-material';
-import { useState } from 'react';
 import { useColorMode } from '../../context/colorModeContext';
 
 function ChangeModeButton() {
-const {isDark,toggleColorMode} = useColorMode();
-    const handleMode = ()=>{
-      document.documentElement.classList.toggle('dark');
-      toggleColorMode();
-    }
+  const { isDark, toggleColorMode } = useColorMode();
+  const handleMode = () => {
+    document.documentElement.classList.toggle('dark');
+    toggleColorMode();
+  }
   return (
-    <button className='fixed right-10 p-2 rounded-full top-32 shadow-gray-700 dark:shadow-gray-500 shadow-lg dark:bg-black  dark:border-white bg-yellow-500 hover:bg-orange-500 hover:top-[126px] z-50'  onClick={handleMode} >
-    {isDark ? <Brightness2Outlined  sx={{ fontSize: '30px' ,color:'white' }} /> : <Brightness7Rounded htmlColor='white' sx={{ fontSize: '30px' }} />}
-  </button>
+    <button className='fixed right-10 p-2 rounded-full top-32 shadow-gray-700 dark:shadow-gray-500 shadow-lg dark:bg-black  dark:border-white bg-yellow-500 hover:bg-orange-500 hover:top-[126px] z-50' onClick={handleMode} >
+      {isDark ? <Brightness2Outlined sx={{ fontSize: '30px', color: 'white' }} /> : <Brightness7Rounded htmlColor='white' sx={{ fontSize: '30px' }} />}
+    </button>
   )
 }
 
