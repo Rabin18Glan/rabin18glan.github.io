6686ca0890df7b8a6c5be5a4f41581c604288f44
rabin18glan <robinjsl321@gmail.com>
2026-02-17 12:39:48 +0545

    refactor: deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Set this to your repository name if not a clean .github.io repo, e.g., '/repo-name/'
 })
